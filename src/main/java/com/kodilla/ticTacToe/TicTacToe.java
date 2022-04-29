5bb9057ba87049761d6d5e9543cfe6e7aac32a86
Kamil Janusz <kamiljanusztc@gmail.com>
2022-04-29 19:21:08 +0200

    Add mouse event

--- src/main/java/com/kodilla/ticTacToe/TicTacToe.java
+++ src/main/java/com/kodilla/ticTacToe/TicTacToe.java
@@ -1,15 +1,21 @@
 package com.kodilla.ticTacToe;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TicTacToe extends Application {
 
@@ -36,28 +42,24 @@ public class TicTacToe extends Application {
         grid.setVgap(5.5);
         grid.setBackground(background);
 
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
+        primaryStage.setTitle("TicTacToe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
 //        struktura rzedow i przekatnych
 //        TBD
 
         Button button1 = new Button("00");
-        Button button2 = new Button("10");
-        Button button3 = new Button("20");
-        Button button4 = new Button("01");
+        Button button2 = new Button("01");
+        Button button3 = new Button("02");
+        Button button4 = new Button("10");
         Button button5 = new Button("11");
-        Button button6 = new Button("21");
-        Button button7 = new Button("02");
-        Button button8 = new Button("01");
-        Button button9 = new Button("02");
-
-        button1.setPrefSize(150, 150);
-        button2.setPrefSize(150, 150);
-        button3.setPrefSize(150, 150);
-        button4.setPrefSize(150, 150);
-        button5.setPrefSize(150, 150);
-        button6.setPrefSize(150, 150);
-        button7.setPrefSize(150, 150);
-        button8.setPrefSize(150, 150);
-        button9.setPrefSize(150, 150);
+        Button button6 = new Button("12");
+        Button button7 = new Button("20");
+        Button button8 = new Button("21");
+        Button button9 = new Button("22");
 
         grid.add(button1, 0, 0);
         grid.add(button2, 1, 0);
@@ -69,7 +71,36 @@ public class TicTacToe extends Application {
         grid.add(button8, 1, 2);
         grid.add(button9, 2, 2);
 
+        List<Button> buttons = new ArrayList<>();
+        buttons.add(button1);
+        buttons.add(button2);
+        buttons.add(button3);
+        buttons.add(button4);
+        buttons.add(button5);
+        buttons.add(button6);
+        buttons.add(button7);
+        buttons.add(button8);
+        buttons.add(button9);
+
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                Button clickedButton = new Button(String.valueOf(e.getSource()));
+                System.out.println("SPRAWDZENIE" + clickedButton);
+                clickedButton.setText("NOWY");
+                System.out.println("po zmianie" + clickedButton);
+//                button1.setText("X");
+            }
+        };
+
+       for (Button button : buttons) {
+           button.setPrefSize(150, 150);
+           button.addEventFilter(MouseEvent.MOUSE_CLICKED, eventHandler);
+       }
+
 //        zbadanie, ktory button kliknal - dodac on click javafx
+
+
 //        user click i computer click function
 
 //        dodanie krzyzyka lub kolka do buttona
@@ -88,12 +119,10 @@ public class TicTacToe extends Application {
 //        crossImg.setFitHeight(100);
 //        crossImg.setFitWidth(100);
 
-        grid.add(buttons, 0, 0, 3, 3);
+//        grid.add(buttons, 0, 0, 3, 3);
 
-        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
 
-        primaryStage.setTitle("TicTacToe");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+
     }
 }
